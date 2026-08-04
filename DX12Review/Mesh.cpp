e8a6f4055140bbd685ff1218aa67dbf2afacebcb
Yoon Ji Hoon <wlgns7922@gmail.com>
2026-08-04 13:40:43 +0900

    사각 큐브 띄우기(인덱스버퍼 O), 와이어프레임 렌더링

--- DX12Review/Mesh.cpp
+++ DX12Review/Mesh.cpp
@@ -9,12 +9,15 @@ CMesh::~CMesh()
 {
 	if (m_pd3dVertexBuffer) m_pd3dVertexBuffer.Reset();
 	if (m_pd3dVertexUploadBuffer) m_pd3dVertexUploadBuffer.Reset();
+	if (m_pd3dIndexBuffer) m_pd3dIndexBuffer.Reset();
+	if (m_pd3dIndexUploadBuffer) m_pd3dIndexUploadBuffer.Reset();
 }
 
 void CMesh::ReleaseUploadBuffers()
 {
-	//정점 버퍼를 위한 업로드 버퍼를 소멸시킨다
+	//업로드 버퍼를 소멸시킨다
 	if (m_pd3dVertexUploadBuffer) m_pd3dVertexUploadBuffer.Reset();
+	if (m_pd3dIndexUploadBuffer) m_pd3dIndexUploadBuffer.Reset();
 }
 
 void CMesh::Render(ID3D12GraphicsCommandList* pd3dCommandList)
@@ -23,8 +26,17 @@ void CMesh::Render(ID3D12GraphicsCommandList* pd3dCommandList)
 	pd3dCommandList->IASetPrimitiveTopology(m_d3dPrimitiveTopology);
 	//메쉬의 정점 버퍼 뷰를 설정한다.
 	pd3dCommandList->IASetVertexBuffers(m_nSlot, 1, &m_d3dVertexBufferView);
-	//메쉬의 정점 버퍼 뷰를 렌더링한다(파이프라인(IA)을 작동하게 한다)
-	pd3dCommandList->DrawInstanced(m_nVertices, 1, m_nOffset, 0);
+
+	//인덱스 버퍼 유무에 따라 다르게 처리
+	if (m_pd3dIndexBuffer)
+	{
+		pd3dCommandList->IASetIndexBuffer(&m_d3dIndexBufferView);
+		pd3dCommandList->DrawIndexedInstanced(m_nIndices, 1, 0, 0, 0);
+	}
+	else
+	{
+		pd3dCommandList->DrawInstanced(m_nVertices, 1, m_nOffset, 0);
+	}
 }
 
 
@@ -66,6 +78,74 @@ CTriangleMesh::~CTriangleMesh()
 CCubeMeshDiffused::CCubeMeshDiffused(ID3D12Device* pd3dDevice, ID3D12GraphicsCommandList* pd3dCommandList, float fWidth, float fHeight, float fDepth)
 	:CMesh(pd3dDevice, pd3dCommandList)
 {
+	m_nVertices = 8;
+	m_nStride = sizeof(CDiffusedVertex);
+	m_d3dPrimitiveTopology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+
+	float fx = fWidth * 0.5f, fy = fHeight * 0.5f, fz = fDepth * 0.5f;
+
+	CDiffusedVertex pVertices[8];
+	int i = 0;
+	//전면 왼쪽 위 0
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(-fx, +fy, -fz), RANDOM_COLOR);
+	//전면 오른쪽 위 1
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(+fx, +fy, -fz), RANDOM_COLOR);
+	//후면 오른쪽 위 2
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(+fx, +fy, +fz), RANDOM_COLOR);
+	//후면 왼쪽 위 3
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(-fx, +fy, +fz), RANDOM_COLOR);
+	//전면 왼쪽 아래 4
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(-fx, -fy, -fz), RANDOM_COLOR);
+	//전면 오른쪽 아래 5
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(+fx, -fy, -fz), RANDOM_COLOR);
+	//후면 오른쪽 아래 6
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(+fx, -fy, +fz), RANDOM_COLOR);
+	//후면 왼쪽 아래 7
+	pVertices[i++] = CDiffusedVertex(XMFLOAT3(-fx, -fy, +fz), RANDOM_COLOR);
+
+	m_pd3dVertexBuffer = ::CreateBufferResource(pd3dDevice, pd3dCommandList, pVertices, m_nStride * m_nVertices, D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_VERTEX_AND_CONSTANT_BUFFER, &m_pd3dVertexUploadBuffer);
+
+	m_d3dVertexBufferView.BufferLocation = m_pd3dVertexBuffer->GetGPUVirtualAddress();
+	m_d3dVertexBufferView.StrideInBytes = m_nStride;
+	m_d3dVertexBufferView.SizeInBytes = m_nStride * m_nVertices;
+
+	//인덱스 버퍼는 직육면체 6개 면에 대한 기하 정보를 가짐. 삼각형 리스트로 표현하므로 정점 인덱스 36(3*2*6)개가 필요
+	m_nIndices = 36;
+
+	UINT pnIndices[36];
+	//앞면 위쪽 삼각형
+	pnIndices[0] = 0, pnIndices[1] = 1, pnIndices[2] = 5;
+	//앞면 아래쪽 삼각형
+	pnIndices[3] = 0, pnIndices[4] = 5, pnIndices[5] = 4;
+	//윗면 위쪽 삼각형
+	pnIndices[6] = 3, pnIndices[7] = 2, pnIndices[8] = 1;
+	//윗면 아래쪽 삼각형
+	pnIndices[9] = 3, pnIndices[10] = 1, pnIndices[11] = 0;
+	//뒷면 위쪽 삼각형
+	pnIndices[12] = 7, pnIndices[13] = 2, pnIndices[14] = 3;
+	//뒷면 아래쪽 삼각형
+	pnIndices[15] = 7, pnIndices[16] = 6, pnIndices[17] = 2;
+	//아래면 위쪽 삼각형
+	pnIndices[18] = 4, pnIndices[19] = 5, pnIndices[20] = 6;
+	//아래면 아래쪽 삼각형
+	pnIndices[21] = 4, pnIndices[22] = 6, pnIndices[23] = 7;
+	//옆면(l) 위쪽 삼각형
+	pnIndices[24] = 3, pnIndices[25] = 0, pnIndices[26] = 4;
+	//옆면(l) 아래쪽 삼각형
+	pnIndices[27] = 3, pnIndices[28] = 4, pnIndices[29] = 7;
+	//옆면(r) 위쪽 삼각형
+	pnIndices[30] = 1, pnIndices[31] = 2, pnIndices[32] = 6;
+	//옆면(r) 아래쪽 삼각형
+	pnIndices[33] = 1, pnIndices[34] = 6, pnIndices[35] = 5;
+
+	m_pd3dIndexBuffer = ::CreateBufferResource(pd3dDevice, pd3dCommandList, pnIndices, sizeof(UINT) * m_nIndices, D3D12_HEAP_TYPE_DEFAULT, D3D12_RESOURCE_STATE_INDEX_BUFFER, &m_pd3dIndexUploadBuffer);
+
+	m_d3dIndexBufferView.BufferLocation = m_pd3dIndexBuffer->GetGPUVirtualAddress();
+	m_d3dIndexBufferView.Format = DXGI_FORMAT_R32_UINT;
+	m_d3dIndexBufferView.SizeInBytes = sizeof(UINT) * m_nIndices;
+
+	/*
+	//아래는 인덱스 버퍼 없이 생성하는 예시임
 	m_nVertices = 36;
 	m_nStride = sizeof(CDiffusedVertex);
 	m_nOffset = 0;
@@ -145,6 +225,7 @@ CCubeMeshDiffused::CCubeMeshDiffused(ID3D12Device* pd3dDevice, ID3D12GraphicsCom
 	m_d3dVertexBufferView.BufferLocation = m_pd3dVertexBuffer->GetGPUVirtualAddress();
 	m_d3dVertexBufferView.StrideInBytes = m_nStride;
 	m_d3dVertexBufferView.SizeInBytes = m_nStride * m_nVertices;
+	*/
 }
 
 CCubeMeshDiffused::~CCubeMeshDiffused()
--- DX12Review/Mesh.h
+++ DX12Review/Mesh.h
@@ -37,16 +37,26 @@ public:
 	virtual void Render(ID3D12GraphicsCommandList* pd3dCommandList);
 
 protected:
-	ComPtr<ID3D12Resource> m_pd3dVertexBuffer = NULL;
-	ComPtr<ID3D12Resource> m_pd3dVertexUploadBuffer = NULL;
+	ComPtr<ID3D12Resource>		m_pd3dVertexBuffer = NULL;
+	ComPtr<ID3D12Resource>		m_pd3dVertexUploadBuffer = NULL;
+	D3D12_VERTEX_BUFFER_VIEW	m_d3dVertexBufferView;
 
-	D3D12_VERTEX_BUFFER_VIEW m_d3dVertexBufferView;
-
+	ComPtr<ID3D12Resource>		m_pd3dIndexBuffer = NULL;
+	ComPtr<ID3D12Resource>		m_pd3dIndexUploadBuffer = NULL;
+	D3D12_INDEX_BUFFER_VIEW		m_d3dIndexBufferView;
+	
 	D3D12_PRIMITIVE_TOPOLOGY m_d3dPrimitiveTopology = D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
 	UINT m_nSlot = 0;
 	UINT m_nVertices = 0;
 	UINT m_nStride = 0;
 	UINT m_nOffset = 0;
+
+	//인덱스 버퍼에 포함되는 인덱스의 개수
+	UINT m_nIndices = 0;
+	//인덱스 버퍼에서 메쉬를 그리기 위해 사용되는 시작 인덱스
+	UINT m_nStartIndex = 0;
+	//인덱스 버퍼의 인덱스에 더해질 인덱스
+	int m_nBaseVertex = 0;
 };
 
 class CTriangleMesh : public CMesh
